fd6ee0942bde89d578a210db2a429a638c769b7c
Persh_vx33@top-academy.ru <Persh_vx33@top-academy.ru>
2024-04-26 17:44:06 +0300

    1

--- My project/main.cpp
+++ My project/main.cpp
@@ -1,16 +1,42 @@
 #include<iostream>
 using namespace std;
-
+using std::cout;
+//#define number_to_the_power
+#define pointers
 void main()
 {
 	setlocale(LC_ALL, "");
-	short i,x = 0;
-	cout << "Введите значение: ";  cin >> i;
-	x = i & 1;
-	cout << (x == 0 ? "Чётное" : "Не чётное");
-	cout << endl;
-	cout << (128 & 1); // AND на чётность, если побитово одно число умножить на один 1 и результат будет 1 то число не чётное, если ноль 0 то число чётное
-	cout << endl;
-	cout << (123 ^ 123); // XOR на равенство чисел, равны числа или нет, если XOR двух чисел равно нулю то числа равны, если  единицы 1 то не равны
+#ifdef number_to_the_power
+
+	double a; //Основание степени
+	int n; //показатель степени
+	double N = 1; //степень
+	cout << "Введите основание степени: "; cin >> a;
+    cout << "Введите показатель степени: "; cin >> n;
+	if (n < 0)
+	{
+		a = 1 / a;
+		n = -n;
+	}
+	for (int i = 0; i < n; i++)
+	{
+		N *= a;
+	}
+	cout << N << endl;
+#endif //  number_to_the_power
+#ifdef pointers
+	int a = 2;
+	int* pa = &a;
+	const int n = 5;
+	int arr[n] = { 3,5,8,13,21 };
+	cout << "pa = " << pa << endl;
+	cout << "*pa = " << *pa << endl;
+	cout << "a = " << a << endl;
+	cout << "&a = " << &a << endl;
+	cout << arr+a << endl;
+	cout << arr << endl;
+
+#endif // pointers
+
 
 }
