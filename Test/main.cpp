37812ad61f993fbccc7b7da94d9e72d33d4f5874
Persh_vx33@top-academy.ru <Persh_vx33@top-academy.ru>
2024-05-08 10:46:25 +0300

    all done

--- Test/main.cpp
+++ Test/main.cpp
@@ -1,8 +1,51 @@
 #include <iostream>
 using namespace std;
 
+#define tab "\t"
+
+void FillRand(int arr[], const int n);
+void Print(int arr[], const int n);
+
 void main()
 {
-	int a = 5;
-	int* b = &a;
+	setlocale(LC_ALL, "");
+	int n;
+	cout << "Введите размер массива: "; cin >> n;
+	int* arr = new int[n];
+
+	FillRand(arr, n);
+	Print(arr, n);
+
+	int value{};//значение элемента
+	int index{};//индекс вставляемого значения
+	int* buffer = new int[n + 1];
+
+	for (int i = 0; i < n; i++)
+	{
+		buffer[i] = arr[i];
+	}
+	Print(arr, n);
+	delete[] arr;
+	buffer[index] = value;
+	n++;
+	delete[] buffer;
+}
+void FillRand(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		//Обращение к элементам массива через арифметику указателей
+		//и оператор разъименования
+		*(arr + i) = rand() % 100;
+	}
+}
+void Print(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		//обращение к элементам массива через 
+		//оператор индексирования - []
+		cout << arr[i] << tab;
+	}
+	cout << endl;
 }
