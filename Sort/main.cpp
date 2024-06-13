98c3d00d9b5c1b7b05aa4e74c4fe4148436682e0
Persh_vx33@top-academy.ru <Persh_vx33@top-academy.ru>
2024-06-13 20:40:44 +0300

    t

--- Sort/main.cpp
+++ Sort/main.cpp
@@ -210,3 +210,17 @@ void insertion_sort(int arr[], const int size)
 		}
 	}
 }
+template <class T>
+void sort(T array[], size_t size)
+{
+	for (int k = size — 1; k > 0; k--)
+	{
+		for (int j = 0; j < k; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(array[j], array[j + 1]);
+			}
+		}
+	}
+}
